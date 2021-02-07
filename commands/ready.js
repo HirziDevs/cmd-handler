5d2683e2c2205797e368e486a20e6765665b199b
Hirzi Gaming YT <64255651+HirziGamingYT@users.noreply.github.com>
2021-02-07 20:52:55 +0700

    Create ready.js

---
+++ commands/ready.js
@@ -0,0 +1,8 @@
+module.exports = async (client) => {
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+  console.log("Name: Custom Bot");
+  console.log("Version: 6.0.0");
+  console.log("Loaded: true");
+  console.log("Created By HirziGamingYT#8701");
+  console.log("=+=+=+=+=+=+=+=+=+=+=+=");
+}
