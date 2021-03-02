35f75e8265be149c00a2db8900fe5b4c66fae744
Hirzi Gaming YT <64255651+HirziGamingYT@users.noreply.github.com>
2021-03-02 10:36:18 +0700

    Create template.js

---
+++ commands/template.js
@@ -0,0 +1,9 @@
+exports.run = (client, message, args) => {
+	const Discord = require("discord.js");
+    const db = require("quick.db");
+try {
+//code here
+} catch (error) {
+      message.channel.send(error)
+    }
+}
